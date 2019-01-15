0622b252f5a2ea1245d621b9b32b6fbfe9cc0299
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2019-01-15 13:19:41 +0700

    Add plain view for income calculator

--- src/screen/incomeCalculator/incomeCalculatorScreen.js
+++ src/screen/incomeCalculator/incomeCalculatorScreen.js
@@ -1,41 +1,70 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text, ScrollView} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 import PropTypes from 'prop-types';
 
 import MainBody from '../../component/mainBody/mainBody.js';
 import Profile from '../../component/profile/profile.js';
-// import LeadList from './leadList/leadList.js';
-// import ScheduleRegistration from './scheduleRegistration/scheduleRegistration.js';
-import styles from './incomeCalculatorScreen.style.js';
 
-// const LeadStackRouter = createStackNavigator(
-//     {
-//       LeadList:LeadList,
-//       Schedule:ScheduleRegistration,
-//     },
-//     {
-//       headerMode:'none',
+import styles, {defaultColor} from './incomeCalculatorScreen.style.js';
 
-//       cardStyle:{backgroundColor:'transparent'}
-//     }
-//   )
+import {createMaterialTopTabNavigator  } from 'react-navigation';
+import { IncomeCalculatorPerYearScreen } from './perYear/incomeCalculatorPerYearScreen.js';
+
+
 
 export default class IncomeCalculatorScreen extends Component{
     // addProps = {
     //     data: this.props.data,
     //     onPress: this.props.onPress,
-    //     navigation:this.props.navigation, 
+    //     navigation:this.props.navigation,
     //     }
 
-    render(){
+    render() {
+            const years = {};
+            for (let i = 1; i <= 5; i++) {
+                years["Year" + i] = {
+                    screen: IncomeCalculatorPerYearScreen,
+                    navigationOptions: ({
+                        navigation
+                    }) => ({
+                        title: "Year " + i
+                    })
+                }
+            }
+
+            const TabNavigator = createMaterialTopTabNavigator(years, {
+                lazy: true,
+                swipeEnabled: true,
+                animationEnabled: true,
+                style: {
+                    backgroundColor: 'white'
+                },
+                navigationOptions: {
+                    gesturesEnabled: true
+                },
+                tabBarOptions: {
+                    showIcon: false,
+                    showLabel: true,
+                    style: {
+                        backgroundColor: 'white'
+                    },
+                    indicatorStyle: {
+                        backgroundColor: defaultColor.Red
+                    },
+                    labelStyle: {
+                        color: 'red'
+                    }
+                },
+            });
+
         return(
             <MainBody source={require('../../../resource/image/bg.jpg')}>
                 <Profile imageOnly={true} source={require('../../../resource/image/profile.jpg')} name="Fandi Fadillah" group="Agency BEST"/>
                 <View style={styles.incomeCalculator_titleContainer}>
                     <Text style={styles.incomeCalculator_titleText}>Income Calculator</Text>
                 </View>
-                
+                <TabNavigator/>
             </MainBody>
         )
     }
--- src/screen/incomeCalculator/incomeCalculatorScreen.style.js
+++ src/screen/incomeCalculator/incomeCalculatorScreen.style.js
@@ -2,6 +2,8 @@ import{ScaledSheet} from 'react-native-size-matters';
 
 import {colors} from '../../helper/style/defaultStyle.js'
 
+export const defaultColor = colors;
+
 export default ScaledSheet.create({
     incomeCalculator_titleContainer:{
         height:'50@vs',
