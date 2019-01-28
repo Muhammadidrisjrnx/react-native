20507641778eb6588f4fa9ab5139f0b9fd8eff29
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2019-01-28 11:14:20 +0700

    Add missing bonus production

---
+++ src/screen/incomeCalculator/perYear/rate/bonusProduction.js
@@ -0,0 +1,47 @@
+export const bonusProductionRate = [
+    {
+        minProduction: 0,
+        maxProduction: 15000000,
+        rate: 0
+    },
+    {
+        minProduction: 15000001,
+        maxProduction: 30000000,
+        rate: 0.02
+    },
+    {
+        minProduction: 30000001,
+        maxProduction: 45000000,
+        rate: 0.03
+    },
+    {
+        minProduction: 45000001,
+        maxProduction: 65000000,
+        rate: 0.05
+    },
+    {
+        minProduction: 65000001,
+        maxProduction: 80000000,
+        rate: 0.06
+    },
+    {
+        minProduction: 80000001,
+        maxProduction: 90000000,
+        rate: 0.08
+    },
+    {
+        minProduction: 90000001,
+        maxProduction: 99999999,
+        rate: 0.09
+    },
+    {
+        minProduction: 90000001,
+        maxProduction: 99999999,
+        rate: 0.09
+    },
+    {
+        minProduction: 100000000,
+        maxProduction: 9999999999999,
+        rate: 0.12
+    },
+]
