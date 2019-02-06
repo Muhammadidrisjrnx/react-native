cab0ed0862f0e64f55113878e7ad6d770a444adf
idrisjrnxgbox01 <idris.xrplsmkrus@gmail.com>
2019-02-06 19:15:28 +0700

    update file introduce

--- src/screen/introduction/introduce.js
+++ src/screen/introduction/introduce.js
@@ -17,8 +17,6 @@ class Circle_icon extends Component{
     render(){
         return(
           <View>
-          <View style={{flexDirection:'column',alignSelf:'center'}}>
-          <View>
           <TouchableOpacity
           onPress={this.onPressButton}  
           style={{
@@ -33,110 +31,6 @@ class Circle_icon extends Component{
               }}>
             <Icon name={"chevron-right"}  size={30} color="#01a699" />
           </TouchableOpacity>
-          </View>
-          <Text>New House</Text>
-          </View>
-          <View style={{position:'relative'}}>
-          <View style={{position:'absolute',marginLeft:20}}>
-          <View>
-          <TouchableOpacity
-          onPress={this.onPressButton}  
-          style={{
-                borderWidth:1,
-                borderColor:'rgba(0,0,0,0.2)',
-                alignItems:'center',
-                justifyContent:'center',
-                width:75,
-                height:75,
-                backgroundColor:'#fff',
-                borderRadius:75,
-              }}>
-            <Icon name={"chevron-right"}  size={30} color="#01a699" />
-          </TouchableOpacity>
-          </View>
-          <Text>New Car</Text>
-          </View>
-          <View style={{alignSelf:'flex-end',marginRight:20}}>
-          <View>
-          <TouchableOpacity
-          onPress={this.onPressButton}  
-          style={{
-                borderWidth:1,
-                borderColor:'rgba(0,0,0,0.2)',
-                alignItems:'center',
-                justifyContent:'center',
-                width:75,
-                height:75,
-                backgroundColor:'#fff',
-                borderRadius:75,
-              }}>
-            <Icon name={"chevron-right"}  size={30} color="#01a699" />
-          </TouchableOpacity>
-          </View>
-          <View style={{marginLeft:10}}>
-          <Text>Education</Text>
-          </View>
-          </View>
-          </View>
-          <View style={{position:'relative',marginTop:30}}>
-          <View style={{position:'absolute',marginLeft:20}}>
-          <View>
-          <TouchableOpacity
-          onPress={this.onPressButton}  
-          style={{
-                borderWidth:1,
-                borderColor:'rgba(0,0,0,0.2)',
-                alignItems:'center',
-                justifyContent:'center',
-                width:75,
-                height:75,
-                backgroundColor:'#fff',
-                borderRadius:75,
-              }}>
-            <Icon name={"chevron-right"}  size={30} color="#01a699" />
-          </TouchableOpacity>
-          </View>
-          <Text>Holiday</Text>
-          </View>
-          <View style={{alignSelf:'flex-end',marginRight:20}}>
-          <View>
-          <TouchableOpacity
-          onPress={this.onPressButton}  
-          style={{
-                borderWidth:1,
-                borderColor:'rgba(0,0,0,0.2)',
-                alignItems:'center',
-                justifyContent:'center',
-                width:75,
-                height:75,
-                backgroundColor:'#fff',
-                borderRadius:75,
-              }}>
-            <Icon name={"chevron-right"}  size={30} color="#01a699" />
-          </TouchableOpacity>
-          </View>
-          <Text>Others</Text>
-          </View>
-          </View>
-          <View style={{flexDirection:'column',alignSelf:'center',marginTop:25,marginBottom:10}}>
-          <View>
-          <TouchableOpacity
-          onPress={this.onPressButton}  
-          style={{
-                borderWidth:1,
-                borderColor:'rgba(0,0,0,0.2)',
-                alignItems:'center',
-                justifyContent:'center',
-                width:75,
-                height:75,
-                backgroundColor:'#fff',
-                borderRadius:75,
-              }}>
-            <Icon name={"chevron-right"}  size={30} color="#01a699" />
-          </TouchableOpacity>
-          </View>
-          <Text>Get Marriage</Text>
-          </View>
           </View>
         );
     }
@@ -158,36 +52,36 @@ export default class introduce_page extends Component {
       <View style={styles.shape_square}>
       <Text style={{marginTop:10,flexDirection:'row',alignSelf:'center'}}>What are the Top Three dreams for your loved ones?</Text>
       <Text style={{marginTop:10,marginBottom:15,color:'#d3d3d3',flexDirection:'row',alignSelf:'center'}}>Drag Max 3 Goals</Text>
-      // <View style={{flexDirection:'column',alignSelf:'center'}}>
+      <View style={{flexDirection:'column',alignSelf:'center'}}>
       <Circle_icon/>
-      // <Text>New House</Text>
-      // </View>
-      // <View style={{position:'relative'}}>
-      // <View style={{position:'absolute',marginLeft:20}}>
-      // <Circle_icon/>
-      // <Text>New Car</Text>
-      // </View>
-      // <View style={{alignSelf:'flex-end',marginRight:20}}>
-      // <Circle_icon/>
-      // <View style={{marginLeft:10}}>
-      // <Text>Education</Text>
-      // </View>
-      // </View>
-      // </View>
-      // <View style={{position:'relative',marginTop:30}}>
-      // <View style={{position:'absolute',marginLeft:20}}>
-      // <Circle_icon/>
-      // <Text>Holiday</Text>
-      // </View>
-      // <View style={{alignSelf:'flex-end',marginRight:20}}>
-      // <Circle_icon/>
-      // <Text>Others</Text>
-      // </View>
-      // </View>
-      // <View style={{flexDirection:'column',alignSelf:'center',marginTop:25,marginBottom:10}}>
-      // <Circle_icon/>
-      // <Text>Get Marriage</Text>
-      // </View>
+      <Text>New House</Text>
+      </View>
+      <View style={{position:'relative'}}>
+      <View style={{position:'absolute',marginLeft:20}}>
+      <Circle_icon/>
+      <Text>New Car</Text>
+      </View>
+      <View style={{alignSelf:'flex-end',marginRight:20}}>
+      <Circle_icon/>
+      <View style={{marginLeft:10}}>
+      <Text>Education</Text>
+      </View>
+      </View>
+      </View>
+      <View style={{position:'relative',marginTop:30}}>
+      <View style={{position:'absolute',marginLeft:20}}>
+      <Circle_icon/>
+      <Text>Holiday</Text>
+      </View>
+      <View style={{alignSelf:'flex-end',marginRight:20}}>
+      <Circle_icon/>
+      <Text>Others</Text>
+      </View>
+      </View>
+      <View style={{flexDirection:'column',alignSelf:'center',marginTop:25,marginBottom:10}}>
+      <Circle_icon/>
+      <Text>Get Marriage</Text>
+      </View>
       <Panel title="Dream 1">
       <View style={{flexDirection:'row'}}>
         <View style={{flexDirection:'column',flex:1}}>
